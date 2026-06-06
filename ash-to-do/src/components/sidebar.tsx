a650d7144a2d238aef9df1a03e193b77d7b4a277
ashera <alexis.variot@epitech.eu>
2026-06-06 11:24:53 +0200

    feature: add side nav bar

--- ash-to-do/src/components/sidebar.tsx
+++ ash-to-do/src/components/sidebar.tsx
@@ -1,9 +1,12 @@
-import { LucideIcon, CheckSquare } from "lucide-react"
+import { LucideIcon, CheckSquare, Calendar } from "lucide-react"
+import { useState } from "react"
 
 export default function Sidebar() {
 
-    function Button({ label, icon: Icon, onClick }: { label: string, icon: LucideIcon, onClick: () => void }) {
-        return <button className="flex justify-start items-center p-2 border-none hover:bg-(--color-surface-elevated)" onClick={onClick}>
+    const [selected, setSelected] = useState<number>(0);
+
+    function Button({ label, icon: Icon, onClick, slt }: { label: string, icon: LucideIcon, onClick: () => void, slt: boolean }) {
+        return <button className={`flex justify-start items-center p-4 border-none ${slt ? 'bg-(--color-surface-elevated)' : '' } hover:bg-(--color-surface-elevated) gap-2 rounded-2xl`} onClick={onClick}>
             <Icon size={16} />
             { label }
         </button>
@@ -15,8 +18,9 @@ export default function Sidebar() {
                 Ash ToDo
             </h1>
         </div>
-        <div className="flex-7 flex flex-col">
-            <Button label={"Todo"} icon={CheckSquare} onClick={() => {}} />
+        <div className="flex-7 flex flex-col p-3 gap-2">
+            <Button label={"Todo"} icon={CheckSquare} onClick={() => {setSelected(0)}} slt={selected == 0} />
+            <Button label={"Calendar"} icon={Calendar} onClick={() => {setSelected(1)}} slt={selected == 1} />
         </div>
         <div className="flex-1">
         </div>
